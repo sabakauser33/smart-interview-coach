351d0f54e59981bdd19d948d8752778ada7d14fa
Saba Kauser <sabakauser33@gmail.com>
2026-04-21 06:54:35 +0530

    update API URLs for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,8 +7,11 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://smart-interview-coach-five.vercel.app/#/login",
     credentials: true
+
+
+    
 }))
 
 /* require all the routes here */
